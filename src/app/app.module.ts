ab889a76802d2cf53de900ecb6a5cdbc955edfbe
Peter Pfrommer <>
2021-03-12 21:29:44 +0100

    latest

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { WeatherComponent } from "./weather/weather.component";
 import { RouterModule } from "@angular/router";
 import { allAppRoutes } from "./routes";
 import { ReactiveFormsModule } from "@angular/forms";
+import { HttpClientModule } from "@angular/common/http";
 
 @NgModule({
   declarations: [AppComponent, WeatherComponent],
   imports: [
     BrowserModule,
     RouterModule.forRoot(allAppRoutes),
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/weather/weather.component.ts
+++ src/app/weather/weather.component.ts
@@ -1,16 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup } from "@angular/forms";
 
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-weather',
   templateUrl: './weather.component.html',
   styleUrls: ['./weather.component.css']
 })
+
+
+
 export class WeatherComponent implements OnInit {
-  public weatherSearchForm: FormGroup;
-  constructor(private formBuilder: FormBuilder) { }
+  public weatherSearchForm!: FormGroup;
+  public weatherData: any;
+
+  constructor(
+    private formBuilder: FormBuilder,
+  ) {}
 
   ngOnInit() {
     this.weatherSearchForm = this.formBuilder.group({
